6d3461ef6ed82e7a22a3f5eec828edb7493b239e
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-01 12:19:49 +0900

    add 오전수업내용2

--- study_0801/src/study_0801/Student.java
+++ study_0801/src/study_0801/Student.java
@@ -4,32 +4,84 @@ public class Student {
 	
 	String name;
 	int age;
-	String phone;
-	//int studentNum = 0;
+	String phoneNum; // int phoneNum = 01012345678 <- int의 크기를 넘어서기 때문에 오류발생
+	int studentNum;
 	
-	Student(String str1, int num1, String str2) {				
-		name = str1;
-		age = num1;
-		phone = str2;
-		//studentNum = 0;				
-	}
+	//s1~s3
+//	Student(String str1, int num1, String str2, int num2) {				
+//		name = str1;
+//		age = num1;
+//		phoneNum = str2;
+//		studentNum = num2;				
+//	}
 	
-	Student(String str1) {				
+	//s4
+	Student(String str1) { // String 하나를 매개변수로 받는 생성자
 		name = str1;
 		age = 0;
-		phone = "010-****-****";			
+		phoneNum = "010-****-****";		
+		
+		if(str1.charAt(0)<58) {
+			phoneNum = str1;
+			name = "Unknown";
+		} else {
+			name = str1;
+			phoneNum = "010-****-****";
+		}
 	}
 	
+	//s5
 	Student(int num) {				
 		name = "Unknown";
 		age = num;
-		phone = "010-****-****";			
+		phoneNum = "010-****-****";			
 	}
 	
-	Student(String str1, int num1) {				
+	//s6 <- s4와 충돌!!
+//	Student(String str2) { // String 하나를 매개변수로 받는 생성자 결국 str1과 str2는 컴퓨터 입장에서 구별을 못한다
+//		name = "Unknown";
+//		age = 0;
+//		phoneNum = str2;			
+//	}
+	
+	//s7
+	Student(String str1, int num) {				
 		name = str1;
-		age = num1;
-		phone = "010-****-****";				
+		age = num;
+		phoneNum = "010-****-****";				
+	}
+	
+	//s8
+	Student(int num, String str2) {				
+		name = "Unknown";
+		age = num;
+		phoneNum = str2;				
+	}
+	
+	Student(String name, int age, String phoneNum) {				
+		//this(); // 클래스 내부(생성자 내부)에서 기본 생성자를 호출
+		//this(name, age, phoneNum); 자기 자신을 불러오는 것은 오류!!
+		this(name, age, phoneNum, 1);
+	}
+	
+	
+	Student(String name, int age, String phoneNum, int studentNum) {				
+		this.name = name;
+		this.age = age;
+		this.phoneNum = phoneNum;
+		this.studentNum = studentNum;				
+	}
+	
+	
+	
+	void printStudent() {
+		System.out.println(this.name + ", " + this.age + "세, " + this.phoneNum + ", 학번: " + this.studentNum);
 	}
 	
 }
+
+//this -> 클래스 내부를 가리킨다. (this.인스턴스변수)
+//Student.printStudent(); 이건 호출이 안된다. 단, this -> 해당 객체
+//this() -> 메소드, 생성자 -> s1();
+//생성자 부분에서 다른 생성자를 불러오는 것.
+//생성자 가장 첫번째 줄에 나와야 한다.
--- study_0801/src/study_0801/class01_practice.java
+++ study_0801/src/study_0801/class01_practice.java
@@ -3,27 +3,55 @@ package study_0801;
 public class class01_practice {
 	public static void main(String[] args) {
 		
-		
 		int num = 1;
 		
-		Student s1 = new Student("홍길동", 27, "010-1234-5678");
-		System.out.println(s1.name + ", " + s1.age + "세, " + s1.phone + ", 학번: " + num++);
-
-		Student s2 = new Student("김철수", 33, "010-1234-5678");
-		System.out.println(s2.name + ", " + s2.age + "세, " + s2.phone + ", 학번: " + num++);
+//		Student s1 = new Student("홍길동", 27, "010-1234-5678");
+//		System.out.println(s1.name + ", " + s1.age + "세, " + s1.phoneNum + ", 학번: " + num++);
+//
+//		Student s2 = new Student("김철수", 33, "010-1234-9876");
+//		System.out.println(s2.name + ", " + s2.age + "세, " + s2.phoneNum + ", 학번: " + num++);
+//		
+//		Student s3 = new Student("이영희", 25, "010-1234-4567");
+//		System.out.println(s3.name + ", " + s3.age + "세, " + s3.phoneNum + ", 학번: " + num++);
+//		
+//		Student s4 = new Student("박나라");
+//		System.out.println(s4.name + ", " + s4.age + "세, " + s4.phoneNum + ", 학번: " + num++);
+//		
+//		Student s5 = new Student(35);
+//		System.out.println(s5.name + ", " + s5.age + "세, " + s5.phoneNum + ", 학번: " + num++);
+//		
+//		Student s6 = new Student("010-1234-3456"); 
+//		System.out.println(s6.name + ", " + s6.age + "세, " + s6.phoneNum + ", 학번: " + num++);
+//		
+//		Student s7 = new Student("최대한", 35);
+//		System.out.println(s7.name + ", " + s7.age + "세, " + s7.phoneNum + ", 학번: " + num++);
+//	
+//		Student s8 = new Student(32, "010-1234-2345");
+//		System.out.println(s8.name + ", " + s8.age + "세, " + s8.phoneNum + ", 학번: " + num++);
 		
-		Student s3 = new Student("이영희", 25, "010-1234-5678");
-		System.out.println(s3.name + ", " + s3.age + "세, " + s3.phone + ", 학번: " + num++);
+		//풀이
 		
-		Student s4 = new Student("박나라");
-		System.out.println(s4.name + ", " + s4.age + "세, " + s4.phone + ", 학번: " + num++);
+		Student s1 = new Student("A", 27, "010-1234-5678", num++);
+		Student s2 = new Student("B", 27, "010-1234-5678", num++);
+		Student s3 = new Student("C", 27, "010-1234-5678", num++);
 		
-		Student s5 = new Student(35);
-		System.out.println(s5.name + ", " + s5.age + "세, " + s5.phone + ", 학번: " + num++);
+		s1.printStudent();
+		s2.printStudent();
+		s3.printStudent();
 		
-		Student s6 = new Student("최대한", 35);
-		System.out.println(s6.name + ", " + s6.age + "세, " + s6.phone + ", 학번: " + num++);
-	
-
+		Student s4 = new Student("010-1234-5678");
+		Student s5 = new Student("jack");
+		// 위 두 경우는 학번이 이어서 증가가 안되는데?
+		
+		//System.out.println((int)'0'); <- 아스키 코드를 찾아서 Student클래스에서 s4번과 같이 확인
+		
+		s4.printStudent();
+		s5.printStudent();
+		
+		Student s6 = new Student("C", 27, "010-1234-5678");
+		s6.printStudent();
+		
+		
 	}	
 }
+
