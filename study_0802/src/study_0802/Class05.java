0ac13c478965210bdc01ce1e49d5f3e0fd942093
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-03 11:10:33 +0900

    add 0803 오전 수업1

--- study_0802/src/study_0802/Class05.java
+++ study_0802/src/study_0802/Class05.java
@@ -20,12 +20,28 @@ public class Class05 {
 		
 		for(int i=0; i<8; i++) {
 			for(int j=0; j<8; j++) {
-				num++;
-				arr[i][j] = num;
+				// num++;
+				arr[i][j] = ++num; // 앞의 num++를 빼고 초기값이 0이니까 ++num을 넣어준다
+									// 초기값이 1이라면 num++를 넣어주면 된다
 				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
 		}
+		
+		System.out.println("깔끔 ----------------------");
+		
+		for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                arr[j][i] = (j*8)+i+1;
+            }
+        }
+		for(int i=0; i<8; i++) {
+			for(int j=0; j<8; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
 		
 		System.out.println();
 		System.out.println("숙제 ----------------------");
@@ -37,19 +53,31 @@ public class Class05 {
 				if((i==1 || i==6) && (j>=1 && j<7)) {
 					num++;
 					arr[i][j] = 0;
-					System.out.print(arr[i][j] + " ");
+					//System.out.print(arr[i][j] + " ");
 				} else if((j==1 || j==6) && (i>=1 && i<7)) {
 					num++;
 					arr[i][j] = 0;
-					System.out.print(arr[i][j] + " ");
+					//System.out.print(arr[i][j] + " ");
 				} else if((i==3||i==4) && (j == 3 || j==4)) {
 					num++;
 					arr[i][j] = 0;
-					System.out.print(arr[i][j] + " ");
+					//System.out.print(arr[i][j] + " ");
 				}
 				else {
 					num++;
 					arr[i][j] = num;
+					//System.out.print(arr[i][j] + " ");
+				}
+			}
+			//System.out.println();
+		}
+		
+		// 위 코드에서 출력도 반복되고 있으니 따로 빼줄 수 있다
+		for(int i=0; i<8; i++) {
+			for(int j=0; j<8; j++) {
+				if(arr[i][j] < 10) {
+					System.out.print(" " + arr[i][j] + " ");
+				} else {
 					System.out.print(arr[i][j] + " ");
 				}
 			}
