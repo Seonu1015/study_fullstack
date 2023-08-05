8f04351bb80e634aa06449007cf0a8072b5448a9
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-05 23:45:50 +0900

    fix DiceGame 2player

--- practice_0805/src/DiceGame_2player/DiceGame.java
+++ practice_0805/src/DiceGame_2player/DiceGame.java
@@ -2,29 +2,17 @@ package DiceGame_2player;
 
 public class DiceGame {
 	public static void main(String[] args) {
-		
+
 		Player p1dice = new Player("대한");
 		Player p2dice = new Player("민국");
-		
-		int score1 = 0;
-		int score2 = 0;
-		
-        int rounds = 6;
 
-        for (int i = 0; i < rounds; i++) {
-            System.out.println("Round " + (i + 1) + ": ");
-            p1dice.playRound(p2dice);
-        }
-		
-		System.out.println("--------- 최종 결과!!! ---------");
-		if (score1 > score2) {
-			System.out.println("축하합니다! " + p1dice.name + "이(가) 승리하였습니다. ^o^");
-		} else if (score1 < score2) {
-			System.out.println("축하합니다! " + p2dice.name + "이(가) 승리하였습니다. ^o^");
-		} else {
-			System.out.println("무승부입니다. o_o;");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Round " + (i + 1) + ": ");
+			p1dice.playRound(p2dice);
 		}
-		
+
+		System.out.println("--------- 최종 결과!!! ---------");
+		p1dice.determineWinner(p2dice);
 	}
 }
 
--- practice_0805/src/DiceGame_2player/Player.java
+++ practice_0805/src/DiceGame_2player/Player.java
@@ -1,40 +1,53 @@
 package DiceGame_2player;
 
+import java.util.Scanner;
+
 public class Player {
-	
+
 	String name;
 	int dice;
 	int score;
-	
+	int roundCount;
+
 	Player(String name) {
 		this.name = name;
 	}
-	
+
 	void printDice() {
-		dice = (int) (Math.random()*6) +1;
+		dice = (int) (Math.random() * 6) + 1;
 		System.out.println(this.name + "의 주사위 : " + dice);
 	}
-	
+
 	int rollDice() {
 		dice = (int) (Math.random() * 6) + 1;
-        return dice;
+		return dice;
 	}
-	
-	void playRound(Player player2) {
-        int player1DiceValue = rollDice();
-        int player2DiceValue = player2.rollDice();
 
-        System.out.println(name + "의 주사위 값: " + player1DiceValue);
-        System.out.println(player2.name + "의 주사위 값: " + player2DiceValue);
+	void playRound(Player p2) {
+		int p1DiceValue = rollDice();
+		int p2DiceValue = p2.rollDice();
 
-        if (player1DiceValue > player2DiceValue) {
-            System.out.println(name + "이(가) 승리했습니다!");
-            score++;
-        } else if (player1DiceValue < player2DiceValue) {
-            System.out.println(player2.name + "이(가) 승리했습니다!");
-            player2.score++;
-        } else {
-            System.out.println("무승부입니다!");
-        }
-    }
+		System.out.println(name + "의 주사위 값: " + p1DiceValue);
+		System.out.println(p2.name + "의 주사위 값: " + p2DiceValue);
+
+		if (p1DiceValue > p2DiceValue) {
+			System.out.println(name + " 이(가) 이겼습니다!");
+			score++;
+		} else if (p1DiceValue < p2DiceValue) {
+			System.out.println(p2.name + " 이(가) 이겼습니다!");
+			p2.score++;
+		} else {
+			System.out.println("무승부입니다.");
+		}
+	}
+
+	void determineWinner(Player p2) {
+		if (score > p2.score) {
+			System.out.println("축하합니다! " + name + " 이(가) 승리하였습니다. ^o^");
+		} else if (score < p2.score) {
+			System.out.println("축하합니다! " + p2.name + " 이(가) 승리하였습니다. ^o^");
+		} else {
+			System.out.println("무승부입니다. o_o;");
+		}
+	}
 }
