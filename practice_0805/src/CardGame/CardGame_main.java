9cab73e131f45883c1b4a1976268ea6f338c5604
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-05 23:11:47 +0900

    fix CardGame 완료

--- practice_0805/src/CardGame/CardGame_main.java
+++ practice_0805/src/CardGame/CardGame_main.java
@@ -5,10 +5,6 @@ import java.util.Scanner;
 public class CardGame_main {
 	public static void main(String[] args) {
 
-		int score1 = 0;
-		int score2 = 0;
-		int hr = 0;
-
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("어떤 대결을 원하십니까? (1. User VS User, 2. User VS Computer, 3. Computer VS Computer)");
@@ -21,6 +17,8 @@ public class CardGame_main {
 		// 그동안 계속 카드가 중복된 이유
 		// 매 라운드마다 객체를 새로 생성했기 때문
 		// 이게 지역변수와 전역변수!!!!
+		
+		Score score = new Score();
 
 		switch (caseGame) {
 		case 1:
@@ -37,6 +35,9 @@ public class CardGame_main {
 
 				System.out.println("플레이어1 카드: " + playerCard1);
 				System.out.println("플레이어2 카드: " + playerCard2);
+				
+				score.calScore(playerCard1, playerCard2);
+				//System.out.println("플레이어1 : " + score.getScorePlayer1() + ", 플레이어2 : "+ score.getScorePlayer2());
 
 				System.out.println();
 			}
@@ -51,8 +52,11 @@ public class CardGame_main {
 				String playerCard1 = person.getPersonCard();
 				String playerCard2 = computer1.getComputerCard();
 
-				System.out.println("플레이어 카드: " + playerCard1);
-				System.out.println("컴퓨터 카드: " + playerCard2);
+				System.out.println("플레이어1 카드: " + playerCard1);
+				System.out.println("플레이어2 카드: " + playerCard2);
+				
+				score.calScore(playerCard1, playerCard2);
+				//System.out.println("플레이어1 : " + score.getScorePlayer1() + ", 플레이어2 : "+ score.getScorePlayer2());
 
 				System.out.println();
 			}
@@ -64,12 +68,37 @@ public class CardGame_main {
 				String playerCard1 = computer1.getComputerCard();
 				String playerCard2 = computer2.getComputerCard();
 
-				System.out.println("컴퓨터 카드: " + playerCard1);
-				System.out.println("컴퓨터 카드: " + playerCard2);
+				System.out.println("플레이어1 카드: " + playerCard1);
+				System.out.println("플레이어2 카드: " + playerCard2);
+				
+				score.calScore(playerCard1, playerCard2);
+                //System.out.println("플레이어1 : " + score.getScorePlayer1() + ", 플레이어2 : "+ score.getScorePlayer2());
 
+                if (score.isGameFinished()) {
+                    break;
+                }
 				System.out.println();
 			}
 			break;
 		}
+
+		int finalScore1 = score.getScorePlayer1();
+        int finalScore2 = score.getScorePlayer2();
+
+        if (score.isGameFinished()) {
+            if (score.hr == 1) {
+                System.out.println("최종 승자는 플레이어1입니다!");
+            } else if (score.hr == 2) {
+                System.out.println("최종 승자는 플레이어2입니다!");
+            }
+        } else {
+            if (finalScore1 > finalScore2) {
+                System.out.println("플레이어1이 최종 승자입니다!");
+            } else if (finalScore1 < finalScore2) {
+                System.out.println("플레이어2가 최종 승자입니다!");
+            } else {
+                System.out.println("무승부입니다!");
+            }
+        }
 	}
 }
