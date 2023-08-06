e4080b197b8900af685a35b41aec3c6b95ff7ec1
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-06 17:16:33 +0900

    add 3인이상 주사위게임 작성중

--- practice_0805/src/DiceGame_3more/DicePlayer.java
+++ practice_0805/src/DiceGame_3more/DicePlayer.java
@@ -1,45 +1,27 @@
 package DiceGame_3more;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DicePlayer extends Player {
 
 	Player[] players;
 	int diceValue;
-	ArrayList<Integer> dicePlayerValue;
 	int roundCount;
 
 	DicePlayer() {
 		super();
-		dicePlayerValue = new ArrayList<>();
 	}
 
 	void playGame(int playerCount) {
 		this.players = new Player[playerCount];
 		Player.printPlayersInfo(playerCount);
-		diceRound();
+		this.diceRound();
 		for (int i = 0; i < roundCount; i++) {
 			System.out.println("Round " + (i + 1) + " -----------------");
-			playRound(playerCount);
-			int maxIndex = findMaxValueIndex(dicePlayerValue);
-			if (maxIndex >= 0) {
-				System.out.println(this.players[maxIndex].name + " 이(가) 이겼습니다.");
-			} else {
-				System.out.println("라운드에서 승자가 없습니다.");
-			}
-			dicePlayerValue.clear();
+						
+			// 게임 진행 내용... 흠...
+			
 		}
-
-	}
-
-	void playRound(int playerCount) {
-		for (int i = 0; i < playerCount; i++) {
-			this.rollDice();
-			dicePlayerValue.add(diceValue);
-		}
-		int maxValue = findMaxValueIndex(dicePlayerValue);
-		System.out.println(dicePlayerValue);
 	}
 
 	void rollDice() {
@@ -48,20 +30,10 @@ public class DicePlayer extends Player {
 
 	void diceRound() {
 		Scanner sc = new Scanner(System.in);
+		System.out.println();
 		System.out.println("몇 라운드를 진행하시겠습니까? :");
 		this.roundCount = sc.nextInt();
 	}
 
-	int findMaxValueIndex(ArrayList<Integer> values) {
-		int maxValue = values.get(0);
-		int maxIndex = -1;
-		for (int i = 0; i < values.size(); i++) {
-			int nextValue = values.get(i);
-			if (nextValue > maxValue) {
-				maxValue = nextValue;
-				maxIndex = i;
-			}
-		}
-		return maxIndex;
-	}
+
 }
