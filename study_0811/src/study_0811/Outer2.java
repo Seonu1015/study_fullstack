a11db5b7fc8e89fa5684d86c4e9b6398f27cfcf0
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-11 14:50:21 +0900

    add 오후 수업 내용1

---
+++ study_0811/src/study_0811/Outer2.java
@@ -0,0 +1,12 @@
+package study_0811;
+
+public class Outer2 {
+	static int a;
+	
+	public static class Inner {
+		public void printOuter() {
+			System.out.println("Outer : " + a);
+		}
+	}
+}
+
---
+++ study_0811/src/study_0811/OuterClass.java
@@ -0,0 +1,20 @@
+package study_0811;
+
+public class OuterClass {
+
+	private int a;
+	
+	public void outerMethod() {
+		InnerInterface inner = new InnerInterface() {
+			@Override
+			public void innerMethod() {
+				System.out.println("내부 이너메소드");
+			}
+ 		};
+ 	}
+	
+	private interface InnerInterface {
+		void innerMethod();
+	}
+	
+}
