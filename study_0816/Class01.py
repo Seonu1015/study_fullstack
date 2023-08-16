fbed379ad9261b36ec1ea7130604b4ef469f55e0
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-16 16:01:46 +0900

    파이썬 오후 수업

---
+++ study_0816/Class01.py
@@ -0,0 +1,21 @@
+# 구구단 만들기 실습!!!!!
+
+# x = int(input("홀수단1, 짝수단2 : "))
+# if x == 1:
+#     x = 3
+# for i in range(x, 10, 2):
+#     for j in range(1, 10):
+#         print("%d X %d = %d" %(i, j, i*j))
+#     print()
+
+# x = int(input("홀수단1, 짝수단2 : "))
+# for i in range(2, 10):
+#     if (x == 1 and i % 2 == 0) or (x == 2 and i % 2 == 1):
+#         continue
+#     print("---------------", i, "단---------------")
+#     for j in range(1, 10):
+#         print("%d X %d = %d" % (i, j, i * j))
+#     print()
+
+# 리스트와 튜플!!
+
---
+++ study_0816/Class03.py
@@ -0,0 +1,14 @@
+x = int(input("찾고자 하는 값을 입력해주세요 : "))
+
+a=[1,1,2,2,2,3,3]
+
+print(a.count(x))
+
+# count 함수 원리 구현해보기
+
+a=[1,2,3]
+a = a.reverse()
+print(a) #[3,2,1]
+
+# reverse 함수 원리 구현해보기
+
