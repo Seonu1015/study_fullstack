3e3ec67ab94e49a939007c9a1fbad5b07e0aef71
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-21 13:22:58 +0900

    0821 오전 수업 내용1

--- study_0821/hw.py
+++ study_0821/hw.py
@@ -1,6 +1,6 @@
 example = [[1, 2, 3], [4, [5, 6]], 7, [8, 9]]
 
-# 재귀 함수를 이용해서 [1,2,3,4,5,6,7,8,9] 출력하기
+# 재귀 함수를 이용해서 [1,2,3,4,5,6,7,8,9] 출력하기 <- Class01 첫 문제
 def flatten(data):
     temp = []
     for i in range(len(data)):
@@ -12,3 +12,5 @@ def flatten(data):
     return temp
 
 flatten(example)
+
+# [1,2,3] 까지 출력해서 고정하고 이런 식으로 진행하면....
