e99bfe2cd1ea77e52dc6d94395801786b7f772b6
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-27 16:37:49 +0900

    0827 필요없는 파일 삭제, 숙제 추가 수정

--- study_0822/Class04.py
+++ study_0822/Class04.py
@@ -20,9 +20,17 @@ class Dog(ABC, Animal):  # 다중 상속이 가능!
 
 
 class Golden(Dog):
-    @abstractmethod
     def walk(self):
         print("터벅터벅")
+
+g1 = Golden()
+g1.eat()
+
+d1 = Dog()
+d1.eat()
+
+a1 = Animal()
+a1.eat()
 
 # 인터페이스 왜 없어??
 # 다중 상속이 가능하다. -> 인터페이스가 필요없다.
--- study_0825/hw01.py
+++ study_0825/hw01.py
@@ -1,5 +1,6 @@
 import pickle
 
+
 def write_stud():
     while True:
         name = input("이름을 입력해주세요. (엔터를 누르면 종료) : ")
@@ -19,18 +20,21 @@ def write_stud():
         with open("score_lst.p", "ab") as f:
             pickle.dump(stud_score, f)
 
-def read_stud(): # readline 은 쓸 수가 없음 오류가 계속 발생, 아마 피클에서는 안되는걸지도??
+
+def read_stud():  # readline 은 쓸 수가 없음 오류가 계속 발생, 아마 피클에서는 안되는걸지도??
     try:
         with open("score_lst.p", "rb") as f:
             count = 0
             while True:
                 try:
                     stud_score = pickle.load(f)
-                    print(f"[{count}] 이름 : {stud_score['이름']}, 수학 : {stud_score['수학']}, 과학 : {stud_score['과학']}, 영어 : {stud_score['영어']}")
+                    print(
+                        f"[{count}] 이름 : {stud_score['이름']}, 수학 : {stud_score['수학']}, 과학 : {stud_score['과학']}, 영어 : {stud_score['영어']}")
                 except EOFError:
                     break
     except FileNotFoundError:
         print("파일이 존재하지 않습니다.")
+
 
 def delete_stud():
     try:
@@ -40,14 +44,15 @@ def delete_stud():
             while True:
                 try:
                     stud_score = pickle.load(f)
-                    print(f"[{count}] 이름 : {stud_score['이름']}, 수학 : {stud_score['수학']}, 과학 : {stud_score['과학']}, 영어 : {stud_score['영어']}")
+                    print(
+                        f"[{count}] 이름 : {stud_score['이름']}, 수학 : {stud_score['수학']}, 과학 : {stud_score['과학']}, 영어 : {stud_score['영어']}")
                     count += 1
                     data.append(stud_score)
                 except EOFError:
                     break
 
             sel_del = int(input("삭제할 번호를 입력해주세요. : "))
-            if sel_del < len(data):
+            if 0 <= sel_del < len(data):
                 del data[sel_del]
 
                 with open("score_lst.p", "wb") as f:
@@ -58,6 +63,7 @@ def delete_stud():
                 print("잘못된 번호입니다.")
     except FileNotFoundError:
         print("파일이 존재하지 않습니다.")
+
 
 while True:
     select_menu = int(input("메뉴를 선택해주세요. 1-입력, 2-조회, 3-삭제, 0-종료 : "))
@@ -70,3 +76,5 @@ while True:
     elif select_menu == 0:
         print("종료되었습니다.")
         break
+    else:
+        print("잘못된 메뉴 선택입니다. 다시 선택해주세요.")
