447b2a199a493f07100f6263f14563276ed4f7e4
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-08-28 16:58:28 +0900

    0828 오후 수업 그래프

--- study_0828/Class04.py
+++ study_0828/Class04.py
@@ -8,17 +8,19 @@ import matplotlib as mpl
 mpl.rcParams['font.family'] = 'Malgun Gothic'
 mpl.rcParams['axes.unicode_minus'] = False
 
+
 def read_csv_file(file_path):
     data = []
     with open(file_path, mode='r', encoding='utf-8') as f:
         reader = csv.reader(f)
-        next(reader) # 헤더 건너뛰기
+        next(reader)  # 헤더 건너뛰기
 
         for row in reader:
             name, age, gender, _ = row
             data.append((int(age), gender))
 
     return data
+
 
 # print(read_csv_file('seocho_people.csv'))
 
@@ -28,8 +30,6 @@ def read_csv_file(file_path):
 age_gen = read_csv_file('seocho_people.csv')
 # print(age_gen)
 # print(type(age_gen[0]))
-
-
 
 data = pd.DataFrame({
     'Age': [age_gen[i][0] for i in range(len(age_gen))],
@@ -43,26 +43,57 @@ aver_f_age = data[data['Gender'] == '여성']['Age'].mean()
 # print("남성 평균 나이 :", aver_m_age)
 # print("여성 평균 나이 :", aver_f_age)
 
-x = []
-y = []
+age = []
+gender = []
 
 for i in range(len(age_gen)):
-    x.append(age_gen[i][0])
-    y.append(age_gen[i][1])
+    age.append(age_gen[i][0])
+    gender.append(age_gen[i][1])
 
-for i in y:
-    if i == '남성':
-        color = 'b'
-    else:
-        color = 'm'
+# for i in y:
+#     if i == '남성':
+#         color = 'y'
+#     else:
+#         color = 'm']
 
-plt.scatter(x, y, c=color)
+color = ['y' if i == '남성' else 'm' for i in gender]
 
-plt.text(aver_m_age, '남성', f"남성 평균 나이 : {aver_m_age}")
-plt.text(aver_f_age, '여성', f"여성 평균 나이 : {aver_f_age}")
+plt.scatter(age, gender, c=color)
+
+plt.text(aver_m_age, '남성', f"남성 평균 나이 : {aver_m_age:.2f}")
+plt.text(aver_f_age, '여성', f"여성 평균 나이 : {aver_f_age:.2f}")
 
 plt.title('서초구 성별 나이')
 plt.xlabel('나이')
 plt.ylabel('성별')
 
+plt.show()
+
+print("----------------------------------------")
+
+
+def creat_scatter_plot(data):
+    coordinates_lst = [(age, 0 if gender == '남성' else 1) for age, gender, _ in data]
+    x_coords_lst, y_coords_lst = zip(*coordinates_lst)
+
+    plt.scatter(x_coords_lst, y_coords_lst, c=y_coords_lst, cmap=plt.get_cmap('bwr'))
+
+    plt.title('서초구 주민 인구 - 나이와 성별')
+    plt.yticks([0, 1], ['남성', '여성'])
+    plt.xlabel('나이')
+    plt.ylabel('성별')
+
+def compare_age(data):
+    male_lst = [age for age, gender, _ in data if gender == '남성']
+    female_lst = [age for age, gender, _ in data if gender == '여성']
+
+    aver_male = sum(male_lst) / len(male_lst)
+    aver_female = sum(female_lst) / len(female_lst)
+    print(f"남성 평균 나이 : {aver_male:.2f}")
+    print(f"여성 평균 나이 : {aver_female:.2f}")
+
+data_set = read_csv_file('seocho_people.csv')
+
+creat_scatter_plot(data_set)
+plt.colorbar(label='Gender')
 plt.show()
--- study_0828/Class05.py
+++ study_0828/Class05.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import csv
 
 import matplotlib as mpl
-
 mpl.rcParams['font.family'] = 'Malgun Gothic'
 mpl.rcParams['axes.unicode_minus'] = False
 
@@ -14,9 +13,24 @@ def read_csv_file(file_path):
         next(reader) # 헤더 건너뛰기
 
         for row in reader:
-            name, age, gender, address, _ = row
+            print(row)
+            name, age, gender, address = row
             data.append((int(age), gender, address))
 
     return data
 
-print(read_csv_file("seocho_people_address.csv"))
+# print(read_csv_file("seocho_people_address.csv"))
+
+def creat_scatter_plot(data):
+    coordinates_lst = [(age, 0 if gender == '남성' else 1, address[len(address)-2:len(address)]) for age, gender, address in data]
+    x_coords_lst, y_coords_lst, z_coords_lst = zip(*coordinates_lst)
+
+    plt.scatter(z_coords_lst, x_coords_lst, c=y_coords_lst, cmap=plt.get_cmap('bwr'), alpha=0.5)
+
+    plt.title('서초구 주민 인구 - 주소별 분포')
+    plt.xlabel('주소')
+    plt.ylabel('나이')
+
+data_set = read_csv_file('seocho_people_address.csv')
+creat_scatter_plot(data_set)
+plt.show()
