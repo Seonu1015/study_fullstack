09abcfc14e21b6b146ea22cc1dedd656d6e17fa5
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-09-11 18:31:05 +0900

    0911 오후 수업

---
+++ study_0910/class05/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {LoginForm} from './login';
+
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ study_0910/class05/array.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+export function Array() {
+    const array = [{id:1, text:'Apple'}, {id:2, text:'Banana'}, {id:3, text:'Kiwi'}]
+
+    const list = nuber.map((id) =>
+        <li key={key.toString()}>{text}</li>
+    );
+
+    return (
+        <div>
+            <h1>[ARRAY]</h1>
+            
+        </div>
+
+    );
+}
