df5fe88c6bde610d0a7994fe96e9ee40bcadc3af
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-09-12 17:14:47 +0900

    0912 오후 수업

---
+++ study0912/class01/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Login from './login'
+import List from './array'
+import Lotto from './lotto'
+import Context from './Context'
+import Language from './Language'
+
+function App() {
+  return (
+    <div>
+        <h1>로그인 관련 처리</h1>
+        <Login />
+        <h1>리스트 여러개 만들기</h1>
+        <List />
+        <Lotto></Lotto>
+        <h1>콘텍스트 사용해보기</h1>
+        <Context/>
+        <h1>콘텍스트로 언어 전달</h1>
+        <Language/>
+    </div>
+  );
+}
+
+export default App;
