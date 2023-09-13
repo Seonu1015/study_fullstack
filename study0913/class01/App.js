a6e591322c734889c1d0c70af969dbfea3fbed80
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-09-13 18:27:14 +0900

    0913 오후 수업

---
+++ study0913/class01/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ShoppingList from './ShoppingList.js';
+
+function App() {
+  return (
+    <div>
+        <h1>[SHOPPITNGLIST]</h1>
+        <ShoppingList/>
+        <Cart/>
+    </div>
+  );
+}
+
+export default App;
