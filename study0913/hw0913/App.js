5dabbd8c355ebe177d47d58b85fdf5fb77a8facd
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-09-14 04:23:43 +0900

    0913 숙제

---
+++ study0913/hw0913/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Stopwatch from './Stopwatch'
+import Turnpage from './Turnpage'
+
+function App() {
+  return (
+    <div>
+      <h1>[STOPWATCH]</h1>
+      <Stopwatch/>
+      <h1>[Turn The Page]</h1>
+      <Turnpage/>
+    </div>
+  );
+}
+
+export default App;
