5e198ee4eb6873d73868cf6204af22497d032222
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-09-21 20:20:42 +0900

    0921 보충수업

--- study_0921/DoublyLinkedList.py
+++ study_0921/DoublyLinkedList.py
@@ -1,12 +1,14 @@
+# 이중 연결 리스트
 class Node:
     def __init__(self, data):
         self.data = data
+        self.prev = None
         self.next = None
-    
-class SinglyLinkedList:
+
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
-
+    
     def is_empty(self):
         return self.head is None
     
@@ -20,28 +22,39 @@ class SinglyLinkedList:
             while temp.next:
                 temp = temp.next
             temp.next = newNode
+            newNode.prev = temp
 
     def prepend(self, data):
         newNode = Node(data)
         if not self.is_empty():
             newNode.next = self.head
+            self.head.prev = newNode
         self.head = newNode
 
     def delete(self, data):
-        prevNode, currentNode = None, self.head
-
         if not self.is_empty():
             if self.head.data == data:
-                self.head = self.head.next
+                temp = self.head
+                if temp.next: # head 다음 노드가 존재하는 경우에만 prev를 None으로 설정
+                    temp.next.prev = None
+                self.head = temp.next
+                temp.next= None
             else:
-                while currentNode is not None and currentNode.data != data:
+                currentNode = self.head
+                
+                while currentNode and currentNode.data != data:
                     prevNode = currentNode
                     currentNode = currentNode.next
 
-                if currentNode is not None:
-                    prevNode.next = currentNode.next
+                if currentNode:
+                    prevNode = currentNode.prev
+                    nextNode = currentNode.next
 
-# 단일 연결 리스트를 역순으로 뒤집는 함수 만들어보기
+                    if currentNode:
+                        prevNode.next = nextNode
+
+                    if nextNode:
+                        nextNode.prev = prevNode
 
 def print_linked_list(linked_list):
     currentNode = linked_list.head
@@ -50,20 +63,8 @@ def print_linked_list(linked_list):
         currentNode = currentNode.next
     print()
 
-def reverse(linked_list):
-    prev_node = None
-    curr_node = linked_list.head
 
-    while curr_node is not None:
-        temp = curr_node.next
-        curr_node.next = prev_node
-
-        prev_node = curr_node
-        curr_node = temp
-
-    linked_list.head = prev_node
-
-linked_list = SinglyLinkedList()
+linked_list = DoublyLinkedList()
 
 linked_list.append(1)
 linked_list.append(2)
@@ -72,9 +73,5 @@ linked_list.append(3)
 print_linked_list(linked_list)
 
 linked_list.delete(2)
-
-print_linked_list(linked_list)
-
-reverse(linked_list)
 
 print_linked_list(linked_list)
