f6ae75fe5720a027b5dd24591cfe66a6417a92b1
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-09-25 12:19:27 +0900

    0925 오전 수업

--- study_0922/class01/weather/urls.py
+++ study_0922/class01/weather/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from weather import views
-
-app_name = 'weather'
+from .views import IndexView, DetailView
 
 urlpatterns = [
-    path('', views.index),
-    path('detail/<int:city_id>/', views.detail, name='detail'),
+    path('', IndexView.as_view()),    
+    path('detail/<int:city_id>/', DetailView.as_view(), name='detail'),
 ]
+
+
+# 함수형
+
+# from django.urls import path
+# from weather import views
+
+# app_name = 'weather'
+
+# urlpatterns = [
+#     path('', views.index),
+#     path('detail/<int:city_id>/', views.detail, name='detail'),
+# ]
